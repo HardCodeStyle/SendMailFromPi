9b773d341bcd845a19807834b9691eb39ebe5c80
Julien <julienhenning00@gmail.com>
2022-03-13 22:09:53 +0100

    Cleanup code
    Julien Henning

--- SendMailFromPiToPc/src/main/java/SpringBoot/EmailSenderService.java
+++ SendMailFromPiToPc/src/main/java/SpringBoot/EmailSenderService.java
@@ -1,15 +1,12 @@
 package SpringBoot;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.io.File;
-import java.nio.file.FileSystem;
 
 @Service
 public class EmailSenderService {
--- SendMailFromPiToPc/src/main/java/SpringBoot/SpringEmailAttachmentApplication.java
+++ SendMailFromPiToPc/src/main/java/SpringBoot/SpringEmailAttachmentApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+
 import javax.mail.MessagingException;
 
 @SpringBootApplication
@@ -12,12 +13,13 @@ public class SpringEmailAttachmentApplication {
 
     @Autowired
     private EmailSenderService emailSenderService;
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringEmailAttachmentApplication.class,args);
+        SpringApplication.run(SpringEmailAttachmentApplication.class, args);
     }
 
     @EventListener(ApplicationReadyEvent.class)
-    public  void triggerEmail() throws MessagingException {
-        emailSenderService.sendMailWithAttachment("julienhenning00@gmail.com","This ist test", "TEST","Users\\jbdim\\Documents\\Links_Für_Projekt_Raspi_Wichtig_Nicht_Löschen.txt");
+    public void triggerEmail() throws MessagingException {
+        emailSenderService.sendMailWithAttachment("julienhenning00@gmail.com", "This ist test", "TEST", "Users\\jbdim\\Documents\\Links_Für_Projekt_Raspi_Wichtig_Nicht_Löschen.txt");
     }
 }
